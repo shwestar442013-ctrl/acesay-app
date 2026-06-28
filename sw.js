528d36250eee399587665d98008bc1e9d121bcf2
shwestar442013-ctrl <shwestar442013@gmail.com>
2026-06-28 08:14:59 +0630

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,1 @@
+const C='ace-v1';self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html'])));self.skipWaiting();});self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(n=>n!==C).map(n=>caches.delete(n)))));self.clients.
